332d18e9eefb595302dbd9116c85664dc7a6f007
Daniel <dolaes@ucsc.edu>
2024-11-14 15:52:42 -0800

    Edit

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -2,13 +2,14 @@ import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
 
-import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
+(async () => {
+  // Dynamically import the JSON data
+  const { default: pythonQuestions } = await import('./pythonQuestions.json');
 
-db.once('open', async () => {
-  await cleanDB('Question', 'questions');
-
-  await Question.insertMany(pythonQuestions);
-
-  console.log('Questions seeded!');
-  process.exit(0);
-});
+  db.once('open', async () => {
+    await cleanDB('Question', 'questions');
+    await Question.insertMany(pythonQuestions);
+    console.log('Questions seeded!');
+    process.exit(0);
+  });
+})();
