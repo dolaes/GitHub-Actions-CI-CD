8cbca007b9e7418d05504339d8bfaaa975149536
Daniel <dolaes@ucsc.edu>
2024-11-14 18:03:15 -0800

    edit

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 // import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
+import { seedAll } from './seeds/seed.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(express.static('../client/dist'));
 
 app.use(routes);
+
+await seedAll();
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
